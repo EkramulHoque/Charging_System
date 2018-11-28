69225e4559db3b9d48dc180af2a9f8f8d65d9cd9
Mehdi <mehdi.nikkhah@gmail.com>
2018-11-28 00:52:35 -0800

    add ne abstract class of Rating

---
+++ Rating_Module/Rating.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+
+class RatingProc(ABC):
+    """
+    abstract class for rating
+    """
+
+    @abstractmethod
+    def __aggregate(self):
+        pass
+
+    @abstractmethod
+    def rate(self):
+        print("RatingProc: Rating")
+        pass
+    @abstractmethod
+    def offer(self):
+        print("RatingProc: Offer")
+        pass
+
