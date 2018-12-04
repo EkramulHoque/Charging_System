090f49b345953eb256291e68a0eb0f0331789dbc
EkramulHoque <ekramulhq26@gmail.com>
2018-12-04 14:55:55 -0800

    removed structure

--- app.py
+++
@@ -1,8 +0,0 @@
-from org.sfu.billing.controller.chargingcontroller import Controller
-
-def main():
-    controller = Controller()
-    controller.process()
-
-if __name__ == "__main__":
-    main()
