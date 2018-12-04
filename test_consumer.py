d253973bb0d4e4491c03658cea7e60e02b462d35
EkramulHoque <ekramulhq26@gmail.com>
2018-12-04 15:14:55 -0800

    Removed all files

--- test_consumer.py
+++
@@ -1,7 +0,0 @@
-import sys
-from kafka import KafkaConsumer
-from definitions import ROOT_DIR, SERVER, TOPIC, CONFIG_FILE
-topic = sys.argv[1]
-consumer = KafkaConsumer(topic, bootstrap_servers=[SERVER])
-for msg in consumer:
-    print(msg.value.decode('utf-8'))
