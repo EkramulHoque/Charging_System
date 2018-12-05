b536b9f0e5ac5a45631bc38e19da24a785a1f211
EkramulHoque <ekramulhq26@gmail.com>
2018-12-04 16:33:09 -0800

    updated config.ini file, configured to localhost ip

--- org/sfu/billing/simulator/controller/producer.py
+++ org/sfu/billing/simulator/controller/producer.py
@@ -1,21 +1,27 @@
 from kafka import KafkaProducer
 import time, threading
+import os
+import configparser
 from org.sfu.billing.simulator.module.datagenerator import generate
-from org.sfu.billing.simulator.utility.data_loader import load_yaml
-from definitions import ROOT_DIR, SERVER, TOPIC, CONFIG_FILE
 
-file_path = ROOT_DIR+CONFIG_FILE
-data = load_yaml(file_path)
-rates = data.get('RATE')# messages per second
+def load_properties():
+    config_dir = os.environ.get('APP_HOME')
+    config_fileName = ('config.ini')
+    config_file = config_dir + os.sep + config_fileName
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    return config
 
+# messages per second
 # creates kafka instance with server name mentioned in the definitions.py
 # creates topic
 # sets frequency of data ingestion
 # gets the message from simulator
 # sends to kafka
 def send_at(rate):
-    producer = KafkaProducer(bootstrap_servers=[SERVER])
-    topic = TOPIC + str(rate)
+    config = load_properties()
+    producer = KafkaProducer(bootstrap_servers=[config['KAFKA']['SERVER_IP']+':'+config['KAFKA']['SERVER_PORT']])
+    topic = config['KAFKA']['TOPIC_PREFIX'] + str(rate)
     interval = 1 / rate
     while True:
         msg = generate()
@@ -24,6 +30,9 @@ def send_at(rate):
 
 #uses thread to produce data for each frequency mentioned in the config.yaml
 if __name__ == "__main__":
+    config = load_properties()
+    rates = map(int, config['KAFKA']['RATE'].split(" "))
+    #rates = [1,10,100]
     for rate in rates:
         server_thread = threading.Thread(target=send_at, args=(rate,))
         server_thread.setDaemon(True)
