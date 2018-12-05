a0717144aa96377a76b64c8fa18117fe722a636b
Mehdi <mehdi.nikkhah@gmail.com>
2018-12-04 21:34:34 -0800

    dataLoader: make the file working as singleton

--- org/sfu/billing/rating/dataLayer.py
+++ org/sfu/billing/rating/dataLayer.py
@@ -1,50 +1,75 @@
 import sys
-import spark
+from org.sfu.billing.utils.configurations import SparkConfig
+
 """
 This is class is used to provide the required functions to load and save data to and from database.
 I implement  the singleton structure in this class.
 
 """
+def singleton(class_):
+    class class_w(class_):
+        _instance = None
+        def __new__(class_, *args, **kwargs):
+            if class_w._instance is None:
+                class_w._instance = super(class_w, class_).__new__(class_,*args,**kwargs)
+                class_w._instance._sealed = False
+            return class_w._instance
+        def __init__(self, *args, **kwargs):
+            if self._sealed:
+                return
+            super(class_w, self).__init__(*args, **kwargs)
+            self._sealed = True
+    class_w.__name__ = class_.__name__
+    return class_w
 
-class dataLoader2:
+@singleton
+class dataLoader:
 
     # Here will be the instance stored.
-    __instance = False
+    __instance = None
     __customers = None
     __offers = None
 
-    @staticmethod
-    def loadCustomers():
+    def loadCustomers(self):
         """ Static access method. """
         try:
-            if not dataLoader2.__instance:
-                dataLoader2()
-            return dataLoader2.__customers
+            print(self.__customers.show(10))
+            return self.__customers
         except:
             print("Unexpected error:", sys.exc_info()[0])
             raise
-    @staticmethod
-    def loadOffers():
+
+
+    def loadOffers(self):
         """ Static access method. """
         try:
-            if not dataLoader2.__instance:
-                dataLoader2()
-            return dataLoader2.__offers
+            print(self.__offers.show(10))
+            return self.__offers
         except:
             print("Unexpected error:", sys.exc_info()[0])
             raise
+
 
     def __init__(self):
         """ Virtually private constructor. """
-        if not dataLoader2.__instance:
+        if  self.__instance==None:
             try:
-                dataLoader2.__customers = "customers"
-                dataLoader2.__offers = 'offers'
-                dataLoader2.__instance = True
+                spark = SparkConfig.get_spark()
+                self.__customers = spark.read.format("com.mongodb.spark.sql.DefaultSource") \
+                    .option("collection", "customers")\
+                    .load()
+                self.__customers.printSchema()
+                self.__offers = spark.read.format("com.mongodb.spark.sql.DefaultSource")\
+                    .option("collection","offers").load()
+                self.__instance = True
             except:
                 print("Unexpected error:", sys.exc_info()[0])
-                raise
+                raise Exception("This is a Spark reading from MongoDb Exception ")
         else:
-            raise Exception("This class is a singleton!")
+            raise Exception("This class is a singleton Exception")
 
 
+if __name__== "__main__":
+    dl=dataLoader()
+    customers = dl.loadCustomers()
+    print(customers.show(10))
