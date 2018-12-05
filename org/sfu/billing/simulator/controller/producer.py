21c9e95180c1148579b3392983c1a000fdfca086
EkramulHoque <ekramulhq26@gmail.com>
2018-12-04 21:36:23 -0800

    message produced on single instance

--- org/sfu/billing/simulator/controller/producer.py
+++ org/sfu/billing/simulator/controller/producer.py
@@ -1,8 +1,9 @@
 from kafka import KafkaProducer
-import time, threading
+import time, threading, random
 import os
 import configparser
 from org.sfu.billing.simulator.module.datagenerator import generate
+from org.sfu.billing.simulator.utility.data_loader import load_customer
 
 def load_properties():
     config_dir = os.environ.get('APP_HOME')
@@ -18,18 +19,32 @@ def load_properties():
 # sets frequency of data ingestion
 # gets the message from simulator
 # sends to kafka
+
+def test_streaming():
+    data = load_customer()
+    count = len(data)
+    while True:
+        index = random.randint(0, count - 1)
+        msg = generate(data[index])
+        print(msg)
+
+
 def send_at(rate):
     config = load_properties()
     producer = KafkaProducer(bootstrap_servers=[config['KAFKA']['SERVER_IP']+':'+config['KAFKA']['SERVER_PORT']])
     topic = config['KAFKA']['TOPIC_PREFIX'] + str(rate)
     interval = 1 / rate
+    data = load_customer()
+    count = len(data)
     while True:
-        msg = generate()
+        index = random.randint(0, count-1)
+        msg = generate(data[index])
         producer.send(topic, msg.encode('ascii'))
         time.sleep(interval)
 
 #uses thread to produce data for each frequency mentioned in the config.yaml
 if __name__ == "__main__":
+    #test_streaming()
     config = load_properties()
     rates = map(int, config['KAFKA']['RATE'].split(" "))
     #rates = [1,10,100]
--- org/sfu/billing/simulator/module/datagenerator.py
+++ org/sfu/billing/simulator/module/datagenerator.py
@@ -23,17 +23,16 @@ def datetime_range(start, end, delta):
         yield current
         current += delta
 
-#1. load configurations from config.ini
+#1. load configurations from config.
 #2. Get the size of records i.e "length",event type ,month(mapped to index)
 #3. Create a list of datetime values
 #4. load customer list created from in data_loader
 #5. Create the message adding the call status and event type as the last two parameters
 #6. example output : TESTCUST12,20181101 154530,20181101 164530,49.252121 | -122.893949,49.252814 | -122.896873,2365482589,2365694587,0,1
-def generate():
+def generate(data):
     config = configparser.ConfigParser()
     config_file, config_dir = load_properties()
     config.read(config_file)
-    length = int(config['KAFKA']['RECORDS'])
     event_type = config['KAFKA']['EVENT']
     date = config['KAFKA']['MONTH']
     start = month_converter(date)
@@ -46,28 +45,15 @@ def generate():
     dts = [dt.strftime('%Y%m%d %H%M%S') for dt in
            datetime_range(datetime(2018, start, 1, random.randint(0, 23)), datetime(2018, end, 31, random.randint(0, 23)),
                           timedelta(minutes=random.randint(1, 60)))]
-    data = load_customer()
-    msg = ''
-    for i in range(1,length):
-        for j in data:
-            interval = random.randint(0,len(dts)-2)
-            # msg += str(data[j].id) + ", " \
-            #        + str(dts[interval])+ ", " \
-            #        + str(dts[interval+1]) + ", " \
-            #        + str(data[j].origin) + ", " \
-            #        + str(data[j].destination) + ", " \
-            #        + str(data[j].caller)+ ", " \
-            #        + str(data[j].receiver) + ", " \
-            #        + str(random.randint(0,1)) + ", " \
-            #        + str(event_type) + '\n'
-            msg += str(dts[interval]) + ", " \
-                   + str(dts[interval + 1]) + ", " \
-                   + str(data[j].origin) + ", " \
-                   + str(data[j].destination) + ", " \
-                   + str(data[j].caller) + ", " \
-                   + str(data[j].receiver) + ", " \
-                   + str(random.randint(0, 1)) + ", " \
-                   + str(event_type) + '\n'
+    interval = random.randint(0, len(dts) - 2)
+    msg = str(dts[interval]) + ", " \
+           + str(dts[interval + 1]) + ", " \
+           + str(data.origin) + ", " \
+           + str(data.destination) + ", " \
+           + str(data.caller) + ", " \
+           + str(data.receiver) + ", " \
+           + str(random.randint(0, 1)) + ", " \
+           + str(event_type)
     return msg
 
 # if __name__ == "__main__":
